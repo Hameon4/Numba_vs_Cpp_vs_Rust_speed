1334f25dc06a49d1abd22d3e199981042551c548
Hamad H. Alsheraifi <61793446+Hameon4@users.noreply.github.com>
2022-02-25 11:38:10 +0400

    Update findMax.cpp

--- findMax.cpp
+++ findMax.cpp
@@ -1,12 +1,11 @@
 #include <vector>
-#include <ctime>
 #include <iostream>
-using namespace std;
+#pragma GCC optimize("O1")
 
 
-int findMax(vector<float> a, vector<float> b) {
-	cout << a.size() << " " << b.size();
-	float maxi = 0;
+int findMax(std::vector<int> a, std::vector<int> b) {
+	std::cout << a.size() << " " << b.size() << std::endl;
+	int maxi = 0;
 	for (int i = 0; i < a.size(); i++) {
 		for (int j = 0; j < b.size(); j++) {
 			if (i * j > maxi)
@@ -17,18 +16,16 @@ int findMax(vector<float> a, vector<float> b) {
 }
 
 int main() {
-	vector<float> a, b;
-	for (int i = 0; i < 50000; i++) {
+	std::vector<int> a, b;
+	for (int i = 0; i < 20000; i++) {
 		a.push_back(0.5);
 		b.push_back(0.4);
 	}
-	time_t start, finish;
-	time(&start);
+	clock_t start, end;
+	start = clock();
 	int s = findMax(a, b);
-	time(&finish);
-	cout << endl << difftime(finish, start) << " seconds" << endl;
+	end = clock();
+	double time_taken = double(end - start) / double(CLOCKS_PER_SEC);
+	std::cout << time_taken << " seconds";
 	return 0;
 }
-
-//399960000 when using clock()
-//5 seconds	when using time()
