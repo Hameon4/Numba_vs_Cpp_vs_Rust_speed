1f5d8411f095e0efc243569e4bf2d41571e8666a
Hamad H. Alsheraifi <61793446+Hameon4@users.noreply.github.com>
2022-03-01 20:04:21 +0400

    Update findMax.cpp

--- findMax.cpp
+++ findMax.cpp
@@ -5,11 +5,15 @@
 #pragma GCC target("avx,avx2,fma")
 
 
-int findMax(std::vector<int> a, std::vector<int> b) {
+
+int findMax(std::vector<int> a, std::vector<int> b) 
+{
 	printf("%d %d\n", int(a.size()), int(b.size()));
 	int maxi = 0;
-	for (int i = 0; i < a.size(); ++i) {
-		for (int j = 0; j < b.size(); ++j) {
+	for (int i = 0; i < int(a.size()); ++i) 
+	{
+		for (int j = 0; j < int(b.size()); ++j) 
+		{
 			if (i * j > maxi)
 				maxi = i * j;
 		}
@@ -17,17 +21,19 @@ int findMax(std::vector<int> a, std::vector<int> b) {
 	return maxi;
 }
 
-int main() {
+int main()
+{
 	std::vector<int> a, b;
-	for (int i = 0; i < 20000; ++i) {
+	for (int i = 0; i < 20000; ++i) 
+	{
 		a.emplace_back(0.5);
 		b.emplace_back(0.4);
 	}
 	clock_t start, end;
 	start = clock();
-	int s = findMax(a, b);
+	findMax(a, b);
 	end = clock();
 	double time_taken = double(end - start) / double(CLOCKS_PER_SEC);
-	printf("Time taken: %f seconds", time_taken);
+	printf("Time taken: %lf seconds\n", time_taken);
 	return 0;
 }
